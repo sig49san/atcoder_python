ec0289fe30f42743574202e9db17f7b76bbc574e
sig49san <sibahara3@gmail.com>
2026-04-08 15:40:15 +0900

    answer.pyとinput.txtを初期化

--- answer.py
+++ answer.py
@@ -14,31 +14,5 @@ if os.path.exists('input.txt'):
     # 念のため sys.stdin も差し替えておくと、sys.stdin.read() 等にも対応できます
     sys.stdin = _f
 
+#ここから回答を記載
 #%%
-N = int(input())
-LRs = [list(map(int, input().split())) for _ in range(N)]
-    
-# %%
-saitei = 0
-saikou = 0
-for L, R in LRs:
-    saitei += L
-    saikou += R
-
-if saitei > 0 or saikou < 0:
-    print("No")
-    exit()
-
-#%%
-print("Yes")
-ans = [LR[0] for LR in LRs]
-sum_ans= sum(ans)
-for i in range(N):
-    if sum_ans < 0:
-        ans[i] += min(LRs[i][1] - LRs[i][0], -sum_ans)
-        sum_ans += min(LRs[i][1] - LRs[i][0], -sum_ans)
-    else:
-        break
-    
-print(*ans)
-# %%
